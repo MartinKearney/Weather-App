5e8e5699be05755e5659d606e41e7a7d1e16f4f1
MartinKearney <martin-kearney@hotmail.co.uk>
2020-02-04 23:42:42 +0000

    Server file made production ready

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(express.json({ extended: false }));
 // Define API end point
 app.use('/', require('./routes/index'));
 
+// Serve static assets in production i.e. React
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  // Set catch all route
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 // The envirnoment variable will be used in production
 const PORT = process.env.PORT || 5000;
 
