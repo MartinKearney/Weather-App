30a226032ec8642ea661d43f032170845aa57e8f
MartinKearney <martin-kearney@hotmail.co.uk>
2020-04-13 16:10:06 +0100

    Basic styling for five day grid

--- client/src/components/display/weather/FiveDayTabItem.js
+++ client/src/components/display/weather/FiveDayTabItem.js
@@ -3,11 +3,22 @@ import React from 'react';
 import './FiveDayTabItem.css';
 
 const FiveDayTabItem = ({ data }) => {
+  console.log(data);
   if (data.time === 99) {
-    return <div className='tab-item empty'>Empty</div>;
+    return <div className='tab-item__empty'></div>;
   }
 
-  return <div className='tab-item'>Weather</div>;
+  return (
+    <div className='tab-item__item'>
+      <p className='tab-item__head'>{data.time}</p>
+      <img
+        src={`http://openweathermap.org/img/wn/${data.icon}@2x.png`}
+        alt='Weather Icon'
+      />
+      <p>Temp: {data.temp}&#176;C</p>
+      <p>Wind: {data.wind}</p>
+    </div>
+  );
 };
 
 export default FiveDayTabItem;
