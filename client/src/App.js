61b9ee85b124188b11b646e970cfdf80ae7aaa40
MartinKearney <martin-kearney@hotmail.co.uk>
2020-07-02 02:47:15 +0100

    Improved commenting

--- client/src/App.js
+++ client/src/App.js
@@ -187,7 +187,7 @@ const App = () => {
 
   const getCurrentWeather = async (city) => {
     // reset this to false to prevent repeated calls to api as
-    // this function is called every render if getWeather is 'true'
+    // function is called every render if getWeather is 'true'
     setGetWeather(false);
 
     const current = await axios.get(
